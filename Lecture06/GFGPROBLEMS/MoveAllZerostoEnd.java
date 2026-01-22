ee520db7d0955080487dd9793c2d1566f5cd17d1
YourName <your@email.com>
2026-01-22 11:17:04 +0530

    GFG question solved

---
+++ Lecture06/GFGPROBLEMS/MoveAllZerostoEnd.java
@@ -0,0 +1,16 @@
+/* Move all Zeros to end  GFG PROBLEM */
+package Lecture06.GFGPROBLEMS;
+public class MoveAllZerostoEnd {
+    void pushZerosToEnd(int[] arr){
+        int n = arr.length;
+        int j = 0;
+        for(int i = 0; i < n ; i++){
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+}
