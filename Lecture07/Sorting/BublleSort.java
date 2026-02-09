1faea57818737fb2b3829cca59f36c5a36659ce1
axranja87 <ayushranjan8757@gmail.com>
2026-02-10 00:48:37 +0530

    Lecture 07 add

---
+++ Lecture07/Sorting/BublleSort.java
@@ -0,0 +1,27 @@
+package Lecture07.Sorting;
+public class BublleSort {
+    public static void SortBublle(int arr[]){
+        int n = arr.length;
+        for(int turn = 0; turn < n - 1; turn++){
+            for(int j = 0; j < n - 1 - turn; j++){
+                if(arr[j] > arr[j + 1 ]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    } 
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,1,6,2};
+        SortBublle(arr);
+        printArr(arr);
+    }
+}
